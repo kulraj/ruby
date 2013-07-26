d15145358ed153268db13382cfb578a75ec8dfb9
kulraj <kulraj@vinsol.com>
2013-07-26 16:42:59 +0530

    vehicles inheritance done

---
+++ 6vehicles.rb
@@ -0,0 +1,36 @@
+<<-doc
+Define a class named 'Vehicle' consisting of 'name','price' and methods for initialising and showing contents. The 'price' of Vehicle may change over time. Now create a subclass 'Bike' having 'dealer' and method to show its content. Initialise a Bike class object with certain values and then change its price
+doc
+
+class Vehicle
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+  def price=(price)
+    @price = price
+  end
+  def show
+    printf("name: %s, price= %d", @name, @price)
+  end
+end
+
+class Bike < Vehicle
+  def initialize(name, price, dealer)
+    super(name, price)
+    @dealer = dealer
+  end
+  def price=(price)
+    super(price)
+  end
+  def show
+    super
+    printf(", dealer: %s\n", @dealer)
+  end
+end
+
+hayabusa = Bike.new("hayabusa", 1000, "lucky")
+hayabusa.show
+hayabusa. price = 2000
+hayabusa.show
+
