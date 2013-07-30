3375e4624b233cf2a126e44bab7df537d3733dc7
kulraj <kulraj@vinsol.com>
2013-07-30 19:00:56 +0530

    code optimized

--- 10hash_inject.rb
+++ 10hash_inject.rb
@@ -5,24 +5,18 @@ Eg: ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ'] -> {"odd"=>[["x", 5], ["ab
 doc
 
 class Array
-  def group_by_even_odd
-    key_length_hash = {}
-    # group items by their length
-    for item in self
-      key_length_hash[item.length] = [] if !key_length_hash.has_key?(item.length)
-      key_length_hash = key_length_hash.merge(Hash[item.length, key_length_hash[item.length].push(item)])
-    end
+  def group_by_even_odd(key_length_hash)
+    key_length_hash = group_by {|item| item.length}
     # group items by even or odd length
-    even_odd_hash = key_length_hash.inject({"odd" => [], "even" => []}) do |even_odd_hash,(key,value)|
-      even_odd_hash["even"].push(value) if key % 2 == 0
-      even_odd_hash["odd"].push(value) if key % 2 == 1
+    key_array = ["even", "odd"]
+    key_length_hash.inject({"odd" => [], "even" => []}) do |even_odd_hash,(key,value)|
+      even_odd_hash[key_array[key % 2]].push(value)
       even_odd_hash
-   end
-   even_odd_hash
+    end
   end
 end
 
 puts "enter the strings separated by whitespaces"
 array = gets.split(" ")
-even_odd_hash = array.group_by_even_odd
+even_odd_hash = array.group_by_even_odd({})
 puts even_odd_hash
