780db0db8a13c3f2fbc0dabded5d0f77a6013313
kulraj <kulraj@vinsol.com>
2013-07-30 15:56:14 +0530

    highlight search result done

---
+++ 16search_result.rb
@@ -0,0 +1,20 @@
+<<-doc
+Write a search method that searches for a letter or word in a user input string and highlights the searched element. Also it should display the total no of searches found.
+
+Eg: If user enters string to be searched as "Can you can a can as a canner can can a can?" and word to be searched is 'can' then output should be:
+(Can) you (can) a (can) as a (can)ner (can) (can) a (can)?
+Total Occurences found: 7
+doc
+
+class String
+  def highlight_and_count(search_word)
+    puts gsub(/#{search_word}/i) {|match| "(" + match + ")"}
+    print "Total occurences found = ", scan(/#{search_word}/i).length, "\n"
+  end
+end
+
+puts "enter string to be searched"
+text_string = gets.chomp
+puts "enter the word to be searched"
+search_word = gets.chomp
+text_string.highlight_and_count(search_word)
