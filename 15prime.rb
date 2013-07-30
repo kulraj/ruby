d381b55a102bf93e32d77fb8d3b2fb24db16f445
kulraj <kulraj@vinsol.com>
2013-07-30 17:44:08 +0530

    step(2)

--- 15prime.rb
+++ 15prime.rb
@@ -4,7 +4,7 @@ require "prime"
 class Integer
   def generate_prime_numbers
     #loop 2 to n and check if the current number is prime?
-    (2..ord).step(1) { |i| print i," " if Prime.prime?(i) }
+    (3..ord).step(2) { |i| print i," " if i.prime? }
   end
 end
 
@@ -13,6 +13,7 @@ n = gets.chomp
 unless n.match(/^\d+$/)
   puts "input is not an integer"
   else
+  print "2 "
   n.to_i.generate_prime_numbers
 end
 
