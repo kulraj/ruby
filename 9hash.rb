3faadad23ddc736700974d7de14493c0bcfc9e50
kulraj <kulraj@vinsol.com>
2013-07-30 19:03:28 +0530

    code optimized

--- 9hash.rb
+++ 9hash.rb
@@ -4,19 +4,12 @@ Eg: ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ'] should give result as {3=>
 doc
 
 class Array
-  def key_length
-    hash = {}
-    for item in self
-      # initialize array for key
-      hash[item.length] = [] if !hash.has_key?(item.length)
-      # append item to value array in hash and merge with existing
-      hash = hash.merge(Hash[item.length, hash[item.length].push(item)])
-    end
-    hash
+  def key_length(hash)
+    group_by { |item| item.length }
   end
 end
 
 puts "enter the strings separated by whitespaces"
 array = gets.split(" ")
-hash = array.key_length
+hash = array.key_length({})
 puts hash
