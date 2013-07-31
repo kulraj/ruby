b972641cd38a29abe27cac9828781aea4364e972
kulraj <kulraj@vinsol.com>
2013-07-31 12:26:15 +0530

    comments added

--- 17sum_time.rb
+++ 17sum_time.rb
@@ -4,13 +4,14 @@ Eg: ("0:45:34","0:15:58") -> 01:01:32 ; ("11:23:07","22:53:45") -> 1 day & 10:16
 doc
 
 def sum_time(first_time, second_time)
+  # initialize the variables
   first_time_array = first_time.split(":")
   second_time_array = second_time.split(":")
   carry = 0
   sum = []
   sum_string = ""
+  # start loop, first add seconds then minutes then hours
   2.downto(0) do |i|
-    #<<-doc
     sum[i] = first_time_array[i].to_i + second_time_array[i].to_i + carry
     case i
     when 0
@@ -26,8 +27,10 @@ def sum_time(first_time, second_time)
         carry = 0
       end
     end
-    sum [i] = sum[i].to_s.rjust(2, '0')
+    # prepend 0 to sum[i] if it has only 1 digit
+    sum[i] = sum[i].to_s.rjust(2, '0')
   end
+  # join sum array into single string and append to existing string
   sum_string.concat(sum.join(":")) 
 end
 
@@ -36,6 +39,7 @@ first_time = gets.chomp
 puts "enter the second time value"
 second_time = gets.chomp
 regex_for_time = /^(([01]?\d)|(2[0-3]))([:]([0-5]\d)){2}$/
+#check for valid time
 unless first_time.match(regex_for_time) && second_time.match(regex_for_time)
   puts "invalid time entered"
   else
