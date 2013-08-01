1a3c57a06e63bd7d285b1ed9914eaecd85b59d31
kulraj <kulraj@vinsol.com>
2013-08-01 10:40:14 +0530

    validation done through class

--- 22name_raise.rb
+++ 22name_raise.rb
@@ -4,25 +4,26 @@ doc
 
 class Name
   def initialize(firstname, lastname)
+    check_null(firstname.strip)
+    raise RuntimeError, "first letter should be capital" if (firstname[0].match(/[a-z]/))
+    check_null(lastname.strip)
     @firstname = firstname
     @lastname = lastname
   end
   def show
     print "welcome ", @firstname, " ", @lastname, "\n"
   end
+  def check_null(name)
+    raise RuntimeError, "null entry" if name == ""
+  end
 end
 
-def check_null(name)
-  raise RuntimeError, "null entry" if name == ""
-end
+
 
 print "enter first name: "
 firstname = gets.chomp
-check_null(firstname.strip)
-raise RuntimeError, "first letter should be capital" if (firstname[0].match(/[a-z]/))
 print "enter last name: "
 lastname = gets.chomp
-check_null(lastname.strip)
 person1 = Name.new(firstname, lastname)
 person1.show
 
