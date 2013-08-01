65fcc973f50795c893d98c7a95aeca0cd61d9fac
kulraj <kulraj@vinsol.com>
2013-08-01 11:53:25 +0530

    reverse iterate done

---
+++ 24reverse_iterate.rb
@@ -0,0 +1,19 @@
+<<-doc
+Implement a reverse iteration function for Array such that I should be able to do this
+[2,4,6,8].reverse_iterate { i print i }
+It would print 8 6 4 2
+Do not use any existing iterator for this.
+doc
+
+class Array
+  def reverse_iterate
+    i = length
+    while i >= 0
+      yield self[i]
+      i -= 1
+    end      
+  end
+end
+
+array = [2,4,6,8]
+array.reverse_iterate { |i| print "#{i} "}
