f793ffa0fd6b4510532250f4e763e8e3f867eee3
kulraj <kulraj@vinsol.com>
2013-08-02 10:35:26 +0530

    object oriented files

--- 23csv/23csv.rb
+++ 23csv/23csv.rb
@@ -1,4 +1,4 @@
-<<-doc
+=begin
 Read a CSV file with the fields Name, EmpId, Designation.
 John, 12, Developer
 Mary, 13, Designer
@@ -13,25 +13,42 @@ Jane (EmpId: 17)
 Developers
 John (EmpId: 12)
 Jack (EmpId: 15)
-doc
+=end
 
 require "csv"
 
-developers_string = "Developers:\n"
-designers_string = "Designers:\n"
+class Employee
+  def initialize(name, id, profile)
+    @name = name
+    @id = id
+    @profile = profile
+  end
+  def to_s
+    "#{@name} (Empid:#{@id})\n"
+  end
+  def profile
+    @profile
+  end
+end
+
+i = 0
+employee = []
+
 CSV.foreach("read_file.csv") do |employee_info|
   # employee_info is one row of the file
   # employee_info[0] contains name, employee_info[1] contains id and employee_info[2] contains the profile
-  employee_info_string = employee_info[0] + " (Empid:" + employee_info[1] + ")\n"
-  # club all developers into one string and all designers into one string
-  if (employee_info[2] =~ /Developer/)
-    developers_string.concat(employee_info_string)
-  else
-    designers_string.concat(employee_info_string)
-  end  
+  # we save the row as an object
+  employee[i] = Employee.new(employee_info[0], employee_info[1], employee_info[2])
+  i += 1
 end
 
-#write the strings to the other file
+# partition by the profile
+employee = employee.partition { |employee| employee.profile =~ /Designer/ }
+
+#write the objects to the other file
 File.open("write_file", "w") do |file|
-  file.print designers_string, "\n", developers_string
+  file.print "Designers:\n"
+  employee[0].each { |employee_i| file.print employee_i.to_s }
+  file.print "\nDevelopers:\n"
+  employee[1].each { |employee_i| file.print employee_i.to_s }
 end
