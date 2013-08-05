5830d7f0853ea709cc081fa9024fdbbc42184349
kulraj <kulraj@vinsol.com>
2013-08-05 11:10:01 +0530

    product initialized in function, check for negative input

--- 13factorial.rb
+++ 13factorial.rb
@@ -1,11 +1,16 @@
 class Integer
-  def factorial(product)
+  def factorial
+    product = 1
     # ord function gives value of the number object, so this is loop from 1 to the number
-    (1..ord).each {|i| product *= i}
+    (1..self).each {|i| product *= i}
     product
   end
 end
 
 puts "enter the number"
 number = gets.to_i
-print "the factorial of ", number, " = ", number.factorial(1),"\n" 
+unless number >= 0
+  puts "negative number entered"
+  else
+  print "the factorial of ", number, " = ", number.factorial,"\n"
+end
