83f23af74d4594af0f43121ed6dbcab6834b4dd0
kulraj <kulraj@vinsol.com>
2013-08-05 17:47:12 +0530

    time parsed and added

--- 17sum_time.rb
+++ 17sum_time.rb
@@ -3,39 +3,36 @@ Write a method that sums up 2 24-hour time values(h:m:s). Validate the time usin
 Eg: ("0:45:34","0:15:58") -> 01:01:32 ; ("11:23:07","22:53:45") -> 1 day & 10:16:52
 doc
 
-def sum_time(first_time, second_time)
-  # initialize the variables
-  first_time_array = first_time.split(":")
-  second_time_array = second_time.split(":")
-  carry = 0
-  sum = []
-  sum_string = ""
-  # start loop, first add seconds then minutes then hours
-  2.downto(0) do |i|
-    sum[i] = first_time_array[i].to_i + second_time_array[i].to_i + carry
-    if i == 0 # hours
-      days = sum[i] / 24
-      sum[i] %= 24
-      sum_string = days.to_s + " day & " if days > 0
-    else # minutes and seconds
-      carry = sum[i] / 60
-      sum[i] %= 60
-    end
-    # prepend 0 to sum[i] if it has only 1 digit
-    sum[i] = sum[i].to_s.rjust(2, '0')
-  end
-  # join sum array into single string and append to existing string
-  sum_string.concat(sum.join(":")) 
+require "time"
+
+def zerofill(time_component)
+  time_component.to_s.rjust(2,'0')
 end
 
-puts "enter the first time value"
+def sum_time(first_timestamp, second_timestamp)
+  hours = second_timestamp.hour + first_timestamp.hour
+  minutes = second_timestamp.min + first_timestamp.min
+  seconds = second_timestamp.sec + first_timestamp.sec
+  minutes += (seconds / 60).to_i
+  hours += (minutes / 60). to_i
+  seconds %= 60
+  minutes %= 60
+  sum_time_string = ""
+  if hours > 24
+    sum_time_string = "#{(hours / 24).to_i} day & " 
+    hours %= 24
+  end
+  sum_time_string += "#{zerofill(hours)}:#{zerofill(minutes)}:#{zerofill(seconds)}"
+end
+
+puts "Enter the first time value"
 first_time = gets.chomp
-puts "enter the second time value"
+puts "Enter the second time value"
 second_time = gets.chomp
 regex_for_time = /^(([01]?\d)|(2[0-3]))([:]([0-5]?\d)){2}$/
 #check for valid time
 unless first_time.match(regex_for_time) && second_time.match(regex_for_time)
-  puts "invalid time entered"
-  else
-  puts sum_time(first_time, second_time)
+  puts "Invalid time entered"
+else
+  puts "Sum of times = #{sum_time(Time.parse(first_time), Time.parse(second_time))}"
 end
