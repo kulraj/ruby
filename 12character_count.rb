78da61849cd9c6dc5c52103823a693cb25e8ce25
kulraj <kulraj@vinsol.com>
2013-08-05 10:57:23 +0530

    names, output format and spacing fixed

--- 12character_count.rb
+++ 12character_count.rb
@@ -6,27 +6,30 @@ doc
 class String
   def character_count
     # ranges defined as per ascii values
-    lowercase = Range.new('a','z')
-    uppercase = Range.new('A','Z')
-    digits = Range.new('0','9')
-    special_characters = Range.new('!','@')
+    lowercase_range = Range.new('a','z')
+    uppercase_range = Range.new('A','Z')
+    digit_range = Range.new('0','9')
+    special_character_range = Range.new('!','@')
     #count initialized
-    no_uppercase = 0
-    no_lowercase = 0
-    no_digits = 0
-    no_special_characters = 0
+    uppercase_count = 0
+    lowercase_count = 0
+    digit_count = 0
+    special_character_count = 0
     self.split("").each do |item|
-      no_uppercase += 1 if uppercase.include?item
-      no_lowercase += 1 if lowercase.include?item
-      no_digits += 1 if digits.include?item
+      uppercase_count += 1 if uppercase_range.include?item
+      lowercase_count += 1 if lowercase_range.include?item
+      digit_count += 1 if digit_range.include?item
       # the defined range for special characters includes digits also so we need to exclude the results for digit 
-      no_special_characters += 1 if special_characters.include?item and !digits.include?item
+      special_character_count += 1 if special_character_range.include?item and !digit_range.include?item
     end
-  return [no_lowercase, no_uppercase, no_digits, no_special_characters]
+    return [lowercase_count, uppercase_count, digit_count, special_character_count]
   end
 end
 
 puts "enter the string: "
 string = gets
 count_array = string.character_count
-puts count_array
+puts "lowercase characters = #{count_array[0]}"
+puts "uppercase characters = #{count_array[1]}"
+puts "digits = #{count_array[2]}"
+puts "special characters = #{count_array[3]}" 
