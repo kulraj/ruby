5fb4311d77d06688484d09fdf58fab948274cd89
kulraj <kulraj@vinsol.com>
2013-08-06 11:14:11 +0530

    optimized

--- 13factorial.rb
+++ 13factorial.rb
@@ -1,14 +1,10 @@
 class Integer
   def factorial
-    unless self >= 0
+    if self < 0
       puts "Negative number entered"
       exit
-    else
-      product = 1
-      # loop from 1 to the number
-      (1..self).each {|i| product *= i}
-      product
     end
+    (1..self).inject(1) {|product, i| product *= i}
   end
 end
 
