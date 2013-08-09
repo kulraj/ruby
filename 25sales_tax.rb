f18d16516d37ac7266b958135dfba43fe7558e4f
kulraj <kulraj@vinsol.com>
2013-08-09 12:15:42 +0530

    invoice class added

--- 25sales_tax.rb
+++ 25sales_tax.rb
@@ -27,37 +27,62 @@ Do you want to add more items to your list(y/n): n
 You have to generate a list that would display the list in a nice format and the grand total should be rounded to the nearest integer
 doc
 
+class Invoice
+  @@item_list = []
+  @@import_duty = 5
+  @@sales_tax = 10
+  
+  def self.display_row(item, max_length_name)
+    #print the data formatted column wise
+    row_string = "| " + item.name.ljust(max_length_name, ' ') + " | "
+    row_string.concat(item.imported.to_s.ljust("imported".length, ' ') + " | ")
+    row_string.concat(item.exempted.to_s.ljust("exempted".length, ' ') + " | ")
+    row_string.concat(item.price.to_s.ljust(" price ".length, ' ') + " | ")
+    print row_string
+  end
+  def self.effective_price(item)
+    #add taxes to the price, round to 2 places after decimal and return 
+    price = item.price
+    price *= 1 + Invoice.sales_tax/100.to_f if item.exempted =~ /no/i
+    price *= 1 + Invoice.import_duty/100.to_f if item.imported =~ /yes/i
+    #Invoice.new(item, price.round(2))
+    @@item_list << self
+    price.round(2)
+  end
+  def self.generate_bill(items, max_length_name)
+    grand_total = 0
+    # we count the spaces before grand total and total columns in our output
+    number_of_spaces = max_length_name + "imported | exempted |  price ".length + "grand total".length
+    number_of_underscores = max_length_name + "  | imported | exempted |  price  | price including tax |  ".length
+    # we use rjust and ljust for formatting the output
+    print " \n".rjust(number_of_underscores, "_")
+    print "| ","name".ljust(max_length_name + 1,' '), "| imported | exempted |  price  | price including tax | \n"
+
+    items.each do |item|
+      display_row(item, max_length_name)
+      effective_price = effective_price(item)
+      print effective_price.to_s.ljust("price including tax".length, ' '), " |\n"
+      grand_total += effective_price
+    end
+    print "\n", ("grand total | " + grand_total.round.to_s).rjust(number_of_spaces, ' '), "\n"
+  end
+end
+
 class Item
+  attr_reader :name, :imported, :exempted, :price
+
   def initialize(name, imported, exempted, price)
     @name = name
     @imported = imported
     @exempted = exempted
     @price = price.to_f
   end
-  def effective_price
-    #add taxes to the price, round to 2 places after decimal and return 
-    price = @price.to_f
-    price *= 1.1 if @exempted =~ /no/i
-    price *= 1.05 if @imported =~ /yes/i
-    price.round(2)
-  end
-  def show(max_length_name)
-    #print the data formatted column wise
-    row_string = "| " + @name.ljust(max_length_name, ' ') + " | "
-    row_string.concat(@imported.to_s.ljust("imported".length, ' ') + " | ")
-    row_string.concat(@exempted.to_s.ljust("exempted".length, ' ') + " | ")
-    row_string.concat(@price.to_s.ljust(" price ".length, ' ') + " | ")
-    print row_string
-  end
-  def name
-    @name
-  end
+  
 end
 
 #initialize the variables
-grand_total = 0
 item_number = 0
-item = []
+items = []
 max_length_name = "name".length
 begin
   # input the details
@@ -73,24 +98,10 @@ begin
   continue_choice = gets.chomp
   #price should be integer
   raise RuntimeError, "entered price is not an integer" if  price !~ /^\d+/
-  item[item_number] = Item.new(name, imported, exempted, price)
+  items[item_number] = Item.new(name, imported, exempted, price)
   # maximum length of name is useful in formatting
-  max_length_name = item[item_number].name.length if max_length_name < item[item_number].name.length
+  max_length_name = items[item_number].name.length if max_length_name < items[item_number].name.length
   item_number += 1
 end while continue_choice =~ /^y$/i
 
-# we count the spaces before grand total and total columns in our output
-number_of_spaces = max_length_name + "imported | exempted |  price ".length + "grand total".length
-number_of_underscores = max_length_name + "  | imported | exempted |  price  | price including tax |  ".length
-
-# we use rjust and ljust for formatting the output
-print " \n".rjust(number_of_underscores, "_")
-print "| ","name".ljust(max_length_name + 1,' '), "| imported | exempted |  price  | price including tax | \n"
-0.upto(item.length - 1) do |i|
-  item[i].show(max_length_name)
-  effective_price = item[i].effective_price
-  print effective_price.to_s.ljust("price including tax".length, ' '), " |\n"
-  grand_total += effective_price
-end
-
-print "\n", ("grand total | " + grand_total.round.to_s).rjust(number_of_spaces, ' '), "\n"
+Invoice.generate_bill(items, max_length_name)
