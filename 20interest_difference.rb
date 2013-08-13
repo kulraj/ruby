878ffd48b7650fa806a1c844e8ee3189957f296e
kulraj <kulraj@vinsol.com>
2013-08-13 17:39:26 +0530

    block passing is optional

--- 20interest_difference.rb
+++ 20interest_difference.rb
@@ -5,14 +5,21 @@ doc
 
 class Interest
   attr_accessor :p, :r, :t
-  def initialize(&block)
-    yield self
+  #default values passed if we do not give argument, if we give argument then that will be valid
+  def initialize(pricipal = 1000, time = 1, &block)
+    if block_given?
+      yield self
+    else
+      self.p = pricipal.to_i
+      self.t = time.to_f
+      self.r = 10
+    end
   end
   def simple_interest
-    @p * @r * @t / 100
+    p * r * t / 100
   end
   def compound_interest
-    @p * (1 + @r.to_f/100) ** @t - @p
+    p * (1 + r.to_f/100) ** t - p
   end
   def difference
     (compound_interest - simple_interest).round(2)
@@ -23,8 +30,9 @@ print "Enter the principal: "
 principal = gets.chomp
 print "Enter the time in years: "
 time = gets.chomp
+regex_for_float = /[+-]?\d+([.]\d+)?/
 # validate principal and time
-unless principal == principal.to_i.to_s && time == time.to_f.to_s
+unless principal == principal.to_i.to_s && time.match(regex_for_float)
   puts "invalid value entered"
   else
   interest = Interest.new do |interest|
